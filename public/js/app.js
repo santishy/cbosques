3bb876ebb93b13916279df4e354bc816a5085acd
santishy <santi_shy@hotmail.com>
2019-11-14 18:30:06 -0600

    desabilitando modal

--- public/js/app.js
+++ public/js/app.js
@@ -4403,20 +4403,23 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
       }]
     };
   },
-  mounted: function mounted() {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then(function (result) {
-      if (result.value) {
-        Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
-      }
-    });
+  mounted: function mounted() {//     Swal.fire({
+    //   title: 'Are you sure?',
+    //   text: "You won't be able to revert this!",
+    //   type: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: 'Yes, delete it!'
+    // }).then((result) => {
+    //   if (result.value) {
+    //     Swal.fire(
+    //       'Deleted!',
+    //       'Your file has been deleted.',
+    //       'success'
+    //     )
+    //   }
+    // })
   },
   mixins: [_mixins_actionsMixin__WEBPACK_IMPORTED_MODULE_1__["default"]],
   components: {
